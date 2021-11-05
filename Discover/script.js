be0c5ca83cdfe69dfad84c5cd1ce809fd477f7b6
Italo Pereira <italopereira007@hotmail.com>
2021-11-04 23:02:30 -0300

    Update script.js

--- Discover/script.js
+++ Discover/script.js
@@ -5,8 +5,10 @@
   * Undefined vs null
 
   * Variaveis:
-    var
+    var - é global e, tambem é local. Ela ira fazer a hosting (elevação) e ira levar a variavel var para o topo.
     let
     const
 
+  * typeof - Ultilizado no console.log ara saber o tipo de uma variavel 
+  
 */
