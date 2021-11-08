760cba4deda5b0d584d126b6492bceab47a39e3e
Italo Pereira <italopereira007@hotmail.com>
2021-11-07 23:56:01 -0300

    .

--- Discover/script.js
+++ Discover/script.js
@@ -1,14 +1,7 @@
-/* 
+// Function expression
+// Function anonymous
+const sum = function(number1, number2) {
+  console.log(number1 + number2);
+}
 
-  Entender melhor sobre:
-
-  * Undefined vs null
-
-  * Variaveis:
-    var - é global e, tambem é local. Ela ira fazer a hosting (elevação) e ira levar a variavel var para o topo.
-    let
-    const
-
-  * typeof - Ultilizado no console.log ara saber o tipo de uma variavel 
-  
-*/
+sum (2, 3); // Argumentos - argumentos
